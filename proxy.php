b8e70f3a5cb9a5adef8db6e1a2d7ea31505998e2
Mathias Sebuuma <mathias240@live.com>
2016-06-25 13:44:30 +0300

    Add files via upload

---
+++ proxy.php
@@ -0,0 +1,9 @@
+<?php 
+$url = "http://212.88.98.116:4050";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec ($ch);
+curl_close ($ch);
+echo $result;
+?>
